5669c79f6f3503a5334fd3ff0ea89174326afa32
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-05-26 17:23:26 +0300

    add user repository

---
+++ src/main/java/com/example/scooter/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.scooter.repository;
+
+import com.example.scooter.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+        Optional<User> findByName(String name);
+
+        boolean existsByName(String name);
+}
+
